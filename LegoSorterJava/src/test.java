c13dfbc0221cce89d487199bd20af592aa65cb69
Max Faber <maxfaber@quicknet.nl>
2017-04-21 13:48:25 +0200

    update

--- LegoSorterJava/src/test.java
+++ LegoSorterJava/src/test.java
@@ -1,8 +1,20 @@
+import java.io.IOException;
+
+import com.hopding.jrpicam.*;
+import com.hopding.jrpicam.enums.Exposure;
+import com.hopding.jrpicam.exceptions.FailedToRunRaspistillException;
+
 public class test
 {
 	public static void main(String[] args)
 	{
-		RPiCamera piCamera = new RPiCamera("/home/pi/Pictures");
+		RPiCamera piCamera = null;
+		try {
+			piCamera = new RPiCamera("/home/pi/Pictures");
+		} catch (FailedToRunRaspistillException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		piCamera.setWidth(500).setHeight(500) // Set Camera to produce 500x500 images.
 	    .setBrightness(75)                // Adjust Camera's brightness setting.
@@ -11,6 +23,14 @@ public class test
 	    .setAddRawBayer(true);            // Add Raw Bayer data to image files created by Camera.
 		// Sets all Camera options to their default settings, overriding any changes previously made.
 		piCamera.setToDefaults();
-		piCamera.takeStill("An Awesome Pic.jpg");
+		try {
+			piCamera.takeStill("An Awesome Pic.jpg");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
