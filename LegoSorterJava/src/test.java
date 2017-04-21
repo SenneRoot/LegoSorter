d22d4b9a8f75741d860c6b18d63fe22bfbf02c72
Max Faber <maxfaber@quicknet.nl>
2017-04-21 13:40:46 +0200

    api_add

--- LegoSorterJava/src/test.java
+++ LegoSorterJava/src/test.java
@@ -2,6 +2,15 @@ public class test
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Test");
+		RPiCamera piCamera = new RPiCamera("/home/pi/Pictures");
+		
+		piCamera.setWidth(500).setHeight(500) // Set Camera to produce 500x500 images.
+	    .setBrightness(75)                // Adjust Camera's brightness setting.
+	    .setExposure(Exposure.AUTO)       // Set Camera's exposure.
+	    .setTimeout(2)                    // Set Camera's timeout.
+	    .setAddRawBayer(true);            // Add Raw Bayer data to image files created by Camera.
+		// Sets all Camera options to their default settings, overriding any changes previously made.
+		piCamera.setToDefaults();
+		piCamera.takeStill("An Awesome Pic.jpg");
 	}
 }
