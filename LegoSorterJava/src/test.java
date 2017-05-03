eec1fb2d778b39e9b84e8c1f1776a78e738d3465
Max Faber <maxfaber@quicknet.nl>
2017-05-03 12:25:56 +0200

    update

--- LegoSorterJava/src/test.java
+++ LegoSorterJava/src/test.java
@@ -1,36 +1,21 @@
-import java.io.IOException;
-
-import com.hopding.jrpicam.*;
+import com.hopding.jrpicam.RPiCamera;
 import com.hopding.jrpicam.exceptions.FailedToRunRaspistillException;
 
+import org.opencv.core.Mat;
+import org.opencv.videoio.VideoCapture;
 
 public class test
 {
+	static VideoCapture capture;
+	static Mat webcam_image;
+	
+	public test()
+	{
+		 capture = new VideoCapture(0);
+		 webcam_image = new Mat();
+	}
 	public static void main(String[] args)
 	{
-		RPiCamera piCamera = null;
-		try {
-			piCamera = new RPiCamera("/home/pi/Pictures");
-		} catch (FailedToRunRaspistillException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		piCamera.setWidth(500).setHeight(500) // Set Camera to produce 500x500 images.
-	    .setBrightness(75)                // Adjust Camera's brightness setting.
-	    .setExposure(Exposure.AUTO)       // Set Camera's exposure.
-	    .setTimeout(2)                    // Set Camera's timeout.
-	    .setAddRawBayer(true);            // Add Raw Bayer data to image files created by Camera.
-		// Sets all Camera options to their default settings, overriding any changes previously made.
-		piCamera.setToDefaults();
-		try {
-			piCamera.takeStill("An Awesome Pic.jpg");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		capture.read(webcam_image);
 	}
 }
