78058e81ccb2777012c8b2ee7b3a50cc7a1a801d
Max Faber <maxfaber@quicknet.nl>
2017-05-12 13:36:01 +0200

    update

--- LegoSorterPython/Regulator.py
+++ LegoSorterPython/Regulator.py
@@ -1,14 +1,16 @@
 import Vibrating_Funnel
 
+
 class Regulator:
-    #def __init__(self):
+    # def __init__(self):
 
-	#def determineColour(self, Snapshot):
+    # def determineColour(self, Snapshot):
 
-	#def getImage(self):
+    # def getImage(self):
 
-	#def runConveyorBelt(self):
+    # def runConveyorBelt(self):
 
-	def runVibratingFunnel(self):
-            Vibrating_Funnel.vibrateFunnel(self, Vibrating_Funnel.speedPWM)
-    #if __name__ == "__main__":
+    def runVibratingFunnel(self):
+
+        Vibrating_Funnel.vibrateFunnel(self, Vibrating_Funnel.speedPWM)
+    # if __name__ == "__main__":
