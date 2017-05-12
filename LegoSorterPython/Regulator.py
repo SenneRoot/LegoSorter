550ea95fa1c4f3a220ffcd7d8be57334a719a1e7
Max Faber <maxfaber@quicknet.nl>
2017-05-12 14:14:34 +0200

    update

--- LegoSorterPython/Regulator.py
+++ LegoSorterPython/Regulator.py
@@ -12,6 +12,7 @@ class Regulator:
 
     def runVibratingFunnel(self):
         #Vibrating_Funnel.vibrateFunnel(self, Vibrating_Funnel.speedPWM)
+        Vibrating_Funnel = Vibrating_Funnel
         Vibrating_Funnel().vibrateFunnel(Vibrating_Funnel(), Vibrating_Funnel().speedPWM)
     if __name__ == "__main__":
         Regulator = Regulator
