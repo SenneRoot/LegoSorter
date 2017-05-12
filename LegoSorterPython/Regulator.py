f3946a41dbccf86520615cd123124251fcbc0851
Max Faber <maxfaber@quicknet.nl>
2017-05-12 14:43:33 +0200

    update

--- LegoSorterPython/Regulator.py
+++ LegoSorterPython/Regulator.py
@@ -11,8 +11,8 @@ class Regulator:
     # def runConveyorBelt(self):
 
     def runVibratingFunnel(self):
-        Vibrating_Funnel.vibrateFunnel(Vibrating_Funnel)
+        Vibrating_Funnel.vibrateFunnel(Vibrating_Funnel())
 
     if __name__ == "__main__":
-        Regulator = Regulator
-        runVibratingFunnel(Regulator)
+        #Regulator = Regulator
+        runVibratingFunnel(Regulator())
