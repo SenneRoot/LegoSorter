6b25cb33de3d67ace8f0b324725983f05151c762
Max Faber <maxfaber@quicknet.nl>
2017-05-12 14:26:45 +0200

    update

--- LegoSorterPython/Vibrating_Funnel.py
+++ LegoSorterPython/Vibrating_Funnel.py
@@ -2,5 +2,5 @@ class Vibrating_Funnel:
     def __init__(self):
         self.funnelID
         self.speedPWM = 10
-    def vibrateFunnel(self, speed):
+    def vibrateFunnel(self):
         print(speedPWM)
