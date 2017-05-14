cdddd151c17cc54344aa7b4ef44425809bca8ed8
Max Faber <maxfaber@quicknet.nl>
2017-05-14 13:16:11 +0200

    update w/ GitKraken

---
+++ hoi/Camera.py
@@ -0,0 +1,7 @@
+class Camera:
+	def __init__(self):
+		self.Camera = Camera
+		self.Resolution = None
+		self.Snapshot = None
+
+	def takeSnapshot(self):
---
+++ hoi/Lego_Sorter.py
@@ -0,0 +1,5 @@
+class Lego_Sorter:
+    def __init__(self):
+        self.Lego_Sorter = Lego_Sorter
+
+    def pushLego(self, Colour):
