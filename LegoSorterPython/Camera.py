ad96f8afec7cca495ff0b7c3e79fec09c4eff28a
Max Faber <maxfaber@quicknet.nl>
2017-05-15 12:30:04 +0200

    udpate

--- LegoSorterPython/Camera.py
+++ LegoSorterPython/Camera.py
@@ -1,8 +1,8 @@
 class Camera:
 	def __init__(self):
-		self.Camera = Camera
-		self.Resolution = None
-		self.Snapshot = None
+		#self.Camera = Camera
+		#self.Resolution = None
+		#self.Snapshot = None
 
 	def takeSnapshot(self):
 		print("Snapshot captured")
