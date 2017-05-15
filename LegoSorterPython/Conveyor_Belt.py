21de39ef4860803a50ece0481f7ccbfd5e6e15ee
Max Faber <maxfaber@quicknet.nl>
2017-05-15 12:21:33 +0200

    update

--- LegoSorterPython/Conveyor_Belt.py
+++ LegoSorterPython/Conveyor_Belt.py
@@ -1,8 +1,8 @@
 class Conveyor_Belt:
 	def __init__(self):
 		self.Conveyor_Belt = Conveyor_Belt
-		self.beltID
-		self.speedPWM
+		#self.beltID
+		#self.speedPWM
 
 	def twistConveyorBelt(self):
 		print("Conveyor Belt is running")
