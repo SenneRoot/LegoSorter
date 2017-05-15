d4680537cdf8e0929062fb10fffea3d3a1285e42
Max Faber <maxfaber@quicknet.nl>
2017-05-15 13:44:46 +0200

    update

--- LegoSorterPython/Camera.py
+++ LegoSorterPython/Camera.py
@@ -5,8 +5,8 @@ class Camera:
 	def __init__(self):
 		self.Camera = picamera.PiCamera()
 		self.Camera.resolution = (1920, 1080)
-		self.Snapshot = None
+		#self.Snapshot = None
 
 	def takeSnapshot(self):
-            Camera.capture('image.jpg')
+        Camera.capture('image.jpg')
         print("Snapshot captured")
