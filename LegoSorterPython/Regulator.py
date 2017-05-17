76557dbc393bd90cadf446d80147e729b7bc0f92
Max Faber <maxfaber@quicknet.nl>
2017-05-17 12:07:17 +0200

    update

--- LegoSorterPython/Regulator.py
+++ LegoSorterPython/Regulator.py
@@ -4,16 +4,53 @@ from Camera import Camera
 
 import picamera
 import Regulator
+import cv2
+import numpy as np
+
+lower_range_rood = np.array([169, 100, 100], dtype=np.uint8)
+upper_range_rood = np.array([189, 255, 255], dtype=np.uint8)
+
+lower_range_geel = np.array([15, 100, 100], dtype=np.uint8)
+upper_range_geel = np.array([35, 255, 255], dtype=np.uint8)
+
+lower_range_blauw = np.array([95, 100, 100], dtype=np.uint8)
+upper_range_blauw = np.array([115, 255, 255], dtype=np.uint8)
+
+lower_range_groen = np.array([64, 100, 100], dtype=np.uint8)
+upper_range_groen = np.array([84, 255, 255], dtype=np.uint8)
 
 class Regulator:
     def __init__(self):
         self.Regulator = Regulator
 
-    #def determineColour(self, Snapshot):
+    def determineColour(self, img):
+		def get_img():
+			import picamera
+			img = cv2.imread("image.jpg", 1)
+			return img
+		def find_color(hsv):
+			if cv2.inRange(hsv, lower_range_rood, upper_range_rood).any():
+				print("dit is rood")
+			elif cv2.inRange(hsv, lower_range_geel, upper_range_geel).any():
+				print("dit is geel")
+			elif cv2.inRange(hsv, lower_range_blauw, upper_range_blauw).any():
+				print("dit is blauw")
+			elif cv2.inRange(hsv, lower_range_groen, upper_range_groen).any():
+				print("dit is groen")
+			else:
+				print("ik weet niet man")
+			
+		def to_hsv(img):
+			hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+			return hsv
 
-    def getImage(camera):
+		img = get_img()
+		hsv = to_hsv(img)
+		find_color(hsv)
+
+	def getImage(camera):
         camera.capture('image.jpg')
-	print("Snapshot captured!")
+		print("Snapshot captured!")
 
     def runConveyorBelt():
         belt1 = Conveyor_Belt()
